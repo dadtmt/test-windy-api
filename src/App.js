98894eab8ede6caff028fbb739592ce56b35295c
Thomas Culdaut <thomas@wildcodeschool.fr>
2020-04-20 17:15:23 +0200

    display webcam

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Iframe from "react-iframe";
 
 function App() {
+  const [webcam, setWebcam] = useState(false);
+  const [hasError, setErrors] = useState(false);
+
+  async function fetchData() {
+    // webcam with id 1499065941
+    const res = await fetch(
+      "https://api.windy.com/api/webcams/v2/list/webcam=1499065941?show=webcams:image,location,player&key=FVKqXhuTWBoicKC5bzKgJW9re2xjxNtN"
+    );
+    res
+      .json()
+      .then((res) => {
+        const { result } = res;
+        const { webcams } = result;
+        const [firstWebcam] = webcams;
+        console.log(firstWebcam);
+        setWebcam(firstWebcam);
+      })
+      .catch((err) => setErrors(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{webcam && <img alt="" src={webcam.image.current.preview} />}</div>
+      <div>{webcam && <Iframe url={webcam.player.lifetime.embed} />}</div>
     </div>
   );
 }
